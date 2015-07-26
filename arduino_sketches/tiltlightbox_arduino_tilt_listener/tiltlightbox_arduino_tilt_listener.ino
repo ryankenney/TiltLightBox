fb241d547a22fcd19749cea1164eb7b6f92b6201
Ryan Kenney <rkenney@moesol.com>
2015-07-26 16:55:15 -0700

    * Added all of the new tilt_light_box.h callbacks to tiltlightbox.ino * Tweaked the tiltlightbox.ino radio configuration to better match the node.js usage * Updated tiltlightbox_arduino_tilt_listener.ino to work with tiltlightbox.ino ** Probably still need to update tiltlightbox_arduino_theme_sender

--- arduino_sketches/tiltlightbox_arduino_tilt_listener/tiltlightbox_arduino_tilt_listener.ino
+++ arduino_sketches/tiltlightbox_arduino_tilt_listener/tiltlightbox_arduino_tilt_listener.ino
@@ -5,35 +5,43 @@
 #define RF_CSN 10
 RF24 radio(RF_CS, RF_CSN);
 const uint64_t pipes[2] = { 0xe7e7e7e7e7LL, 0xc2c2c2c2c2LL };
- 
-struct sensor_struct{
-  int sensor_id;
-  float temp;
-  float soil_temp;
-  float humid;
-  float pres;
-};
 
+void initRadio();
+  
 void setup() {
+  // Init serial printing
   Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(pipes[1]);    // note that our pipes are the same above, but that
-  radio.openReadingPipe(1, pipes[0]); // they are flipped between rx and tx sides.
-  radio.startListening();
-  radio.printDetails();
+  // Init radio
+  initRadio();
 }
 
 void loop() {
   if (radio.available()) {
     uint8_t rx_data[1];
-    bool done = false;
-    while (!done) {
-      done = radio.read( &rx_data, sizeof(rx_data) );
-      Serial.print("Read Tilt (");
+    while (radio.available()) {
+      radio.read( &rx_data, sizeof(rx_data) );
+
+      Serial.print("Recieved tilt message: ");
       Serial.print(rx_data[0]);
-      Serial.print(") ");
-      Serial.print(millis());
       Serial.print("\n");
     }
   }
 }
+
+void initRadio() {
+  radio.begin();
+  
+  // The following settings match our node.js "nrf" usage settings
+  radio.setChannel(0x4c);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setCRCLength(RF24_CRC_16);
+  radio.setRetries(15,15);
+  
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1, pipes[0]);
+  radio.startListening();
+
+  // This was blocking execution on my Uno for some reason
+  //radio.printDetails();
+}
+  
